6d036abf4bcbf13138c9f919c146f2636a54323c
SAM MCCALLIEN <sammccallien@outlook.com>
2026-06-25 13:34:03 +0100

    v3.1

--- setting.py
+++ setting.py
@@ -1,9 +1,8 @@
 
-screen_width = 700
-screen_height = 600
+screen_width = 1840
+screen_height = 950
 black = (0,0,0)
-screen_width = 1200
-screen_height = 600
+
 
 game_title = "Nea game : killer waves: return of the wave kiler volume 6 VII"
 
