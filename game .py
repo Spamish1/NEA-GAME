f2ff757122859df73210d57959bd1d46253a0491
SAM MCCALLIEN <sammccallien@outlook.com>
2026-06-25 11:46:54 +0100

    sortin game file name

---
+++ game .py
@@ -0,0 +1,5 @@
+class game :
+    def__init__(self):
+     pygame.init()
+     self.screen = pygame.display.set_mode((screen_width, screen_height)) 
+     pygame
---
+++ setting.py
@@ -0,0 +1,3 @@
+screen_width = 700
+screen_height = 600
+black = (0,0,0)
